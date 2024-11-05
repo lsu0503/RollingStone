2f6d1449ed02dabe1d9f606e2216d8dd89216c44
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-11-05 13:03:27 +0900

    [Add] Add Base Scripts

---
+++ RollingStone/Assets/Scripts/BaseScript/GenericSingleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GenericSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+
+                if(instance == null)
+                    instance = new GameObject(typeof(T).Name).AddComponent<T>();
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = gameObject.AddComponent<T>();
+        }
+
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject.transform.root);
+    }
+}
